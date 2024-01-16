60e5dd43b104ba815e9513d97d8779f2b5ffdb53
Raven Barrogo <barrogoraven@gmail.com>
2024-01-16 12:26:15 +0800

    wip

--- app.ts
+++ app.ts
@@ -4,39 +4,69 @@ const invalidMsg = document.getElementById('invalid-msg')! as HTMLParagraphEleme
 let timeout: undefined | number;
 
 form.addEventListener('submit', submitForm);
-linkInput.addEventListener('focus', handleInputFocus);
 
 function submitForm(event: SubmitEvent) {
   event.preventDefault();
   clearTimeout(timeout);
 
-  const isInvalid = validateForm(linkInput.value);
-
-  if (isInvalid) {
-    showInvalidMessage();
+  if (isFormEmpty(linkInput.value)) {
+    showInvalidMessage('Please add a link');
     clearInvalidMessage();
     return;
   }
+
+  if (!isValidUrl(linkInput.value)) {
+    showInvalidMessage('URL is not valid');
+    clearInvalidMessage();
+    return;
+  }
+
+  shortenUrl();
 }
 
-function validateForm(value: string): Boolean {
+function isFormEmpty(value: string): Boolean {
   return value.trim() === '';
 }
 
-function showInvalidMessage(): void {
+function isValidUrl(url: string): Boolean {
+  const urlPattern = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+
+  return urlPattern.test(url);
+}
+
+async function shortenUrl() {
+  const url = 'https://cleanuri.com/api/v1/shorten';
+  const payload = {
+    url: 'http://google.com/',
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      // mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    });
+
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function showInvalidMessage(message: string): void {
   linkInput.classList.add('form-invalid');
   invalidMsg.classList.remove('hidden');
+  invalidMsg.textContent = message;
+  linkInput.focus();
 }
 
 function clearInvalidMessage(): void {
   timeout = setTimeout(() => {
     linkInput.classList.remove('form-invalid');
     invalidMsg.classList.add('hidden');
+    invalidMsg.textContent = '';
   }, 3000);
-}
-
-function handleInputFocus() {
-  clearTimeout(timeout);
-  linkInput.classList.remove('form-invalid');
-  invalidMsg.classList.add('hidden');
 }

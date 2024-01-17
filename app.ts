81f81ac13799b255d4e7b3f43c928739f1978b21
Raven Barrogo <barrogoraven@gmail.com>
2024-01-17 11:12:06 +0800

    added url shortener api

--- app.ts
+++ app.ts
@@ -1,7 +1,20 @@
+type Link = {
+  long: string;
+  short: string;
+};
+
 const form = document.getElementById('link-form')! as HTMLFormElement;
 const linkInput = document.getElementById('link-input')! as HTMLInputElement;
+const shortenBtn = document.getElementById('shorten-btn')! as HTMLButtonElement;
 const invalidMsg = document.getElementById('invalid-msg')! as HTMLParagraphElement;
+const linksContainer = document.getElementById('links-container')! as HTMLDivElement;
+
+const API_TOKEN = '28e395216c2b9927cbcfc41c0c58990b65d2cf22';
+const API_URL = 'https://api-ssl.bitly.com/v4/shorten';
+
 let timeout: undefined | number;
+const links: Link[] = displayShortenUrl();
+links.forEach(createShortenUrl);
 
 form.addEventListener('submit', submitForm);
 
@@ -9,23 +22,25 @@ function submitForm(event: SubmitEvent) {
   event.preventDefault();
   clearTimeout(timeout);
 
-  if (isFormEmpty(linkInput.value)) {
-    showInvalidMessage('Please add a link');
-    clearInvalidMessage();
+  const longUrl = linkInput.value.trim();
+
+  if (isFormEmpty(longUrl)) {
+    showInvalidMessage('Please add a link.');
+    linkInput.focus();
     return;
   }
 
-  if (!isValidUrl(linkInput.value)) {
-    showInvalidMessage('URL is not valid');
-    clearInvalidMessage();
+  if (!isValidUrl(longUrl)) {
+    showInvalidMessage('URL is not valid.');
     return;
   }
 
-  shortenUrl();
+  shortenUrl(longUrl);
+  form.reset();
 }
 
 function isFormEmpty(value: string): Boolean {
-  return value.trim() === '';
+  return value === '';
 }
 
 function isValidUrl(url: string): Boolean {
@@ -34,33 +49,12 @@ function isValidUrl(url: string): Boolean {
   return urlPattern.test(url);
 }
 
-async function shortenUrl() {
-  const url = 'https://cleanuri.com/api/v1/shorten';
-  const payload = {
-    url: 'http://google.com/',
-  };
-
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      // mode: 'no-cors',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    });
-
-    console.log(response);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
 function showInvalidMessage(message: string): void {
   linkInput.classList.add('form-invalid');
   invalidMsg.classList.remove('hidden');
   invalidMsg.textContent = message;
-  linkInput.focus();
+
+  clearInvalidMessage();
 }
 
 function clearInvalidMessage(): void {
@@ -69,4 +63,92 @@ function clearInvalidMessage(): void {
     invalidMsg.classList.add('hidden');
     invalidMsg.textContent = '';
   }, 3000);
+}
+
+function showLoadingState() {
+  linkInput.setAttribute('disabled', 'true');
+  linkInput.classList.add('disabled');
+  shortenBtn.setAttribute('disabled', 'true');
+  shortenBtn.classList.add('disabled');
+  shortenBtn.textContent = 'Loading...';
+}
+
+function removeLoadingState() {
+  linkInput.removeAttribute('disabled');
+  linkInput.classList.remove('disabled');
+  shortenBtn.removeAttribute('disabled');
+  shortenBtn.classList.remove('disabled');
+  shortenBtn.textContent = 'Shorten it!';
+}
+
+async function shortenUrl(longUrl: string) {
+  const payload = {
+    long_url: longUrl,
+    domain: 'bit.ly',
+  };
+  const options = {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${API_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  try {
+    showLoadingState();
+    const response = await fetch(API_URL, options);
+
+    if (!response.ok) {
+      throw new Error('Something went wrong. Please try again.');
+    }
+
+    const result = await response.json();
+    const newLinks: Link = {
+      long: result.long_url,
+      short: result.link,
+    };
+
+    links.push(newLinks);
+    saveShortenUrl();
+    createShortenUrl(newLinks);
+    removeLoadingState();
+  } catch (error: unknown) {
+    removeLoadingState();
+    if (error instanceof Error) {
+      showInvalidMessage(error.message);
+    } else {
+      showInvalidMessage('An unknown error occurred.');
+    }
+  }
+}
+
+function createShortenUrl(link: Link): void {
+  const div = document.createElement('div');
+  div.className = 'link-card';
+  div.innerHTML = `
+    <a href="${link.long}" target="_blank" rel="noopener noreferrer" title="${link.long}" class="long-url">
+      ${link.long}
+    </a>
+    <div class="action-container">
+      <a href="${link.short}" target="_blank" rel="noopener noreferrer" title="${link.short}" class="short-url">
+        ${link.short}
+      </a>
+      <button type="button" class="copy-btn">Copy</button>
+    </div>
+      `;
+
+  linksContainer.classList.remove('hidden');
+  const firstChild = linksContainer.firstChild;
+  linksContainer.insertBefore(div, firstChild);
+}
+
+function saveShortenUrl(): void {
+  localStorage.setItem('links', JSON.stringify(links));
+}
+
+function displayShortenUrl(): Link[] {
+  const links = localStorage.getItem('links');
+
+  return links ? JSON.parse(links) : [];
 }
--- dist/app.js
+++ dist/app.js
@@ -1,55 +1,43 @@
 "use strict";
 const form = document.getElementById('link-form');
 const linkInput = document.getElementById('link-input');
+const shortenBtn = document.getElementById('shorten-btn');
 const invalidMsg = document.getElementById('invalid-msg');
+const linksContainer = document.getElementById('links-container');
+const API_TOKEN = '28e395216c2b9927cbcfc41c0c58990b65d2cf22';
+const API_URL = 'https://api-ssl.bitly.com/v4/shorten';
 let timeout;
+const links = displayShortenUrl();
+links.forEach(createShortenUrl);
 form.addEventListener('submit', submitForm);
 function submitForm(event) {
     event.preventDefault();
     clearTimeout(timeout);
-    if (isFormEmpty(linkInput.value)) {
-        showInvalidMessage('Please add a link');
-        clearInvalidMessage();
+    const longUrl = linkInput.value.trim();
+    if (isFormEmpty(longUrl)) {
+        showInvalidMessage('Please add a link.');
+        linkInput.focus();
         return;
     }
-    if (!isValidUrl(linkInput.value)) {
-        showInvalidMessage('URL is not valid');
-        clearInvalidMessage();
+    if (!isValidUrl(longUrl)) {
+        showInvalidMessage('URL is not valid.');
         return;
     }
-    shortenUrl();
+    shortenUrl(longUrl);
+    form.reset();
 }
 function isFormEmpty(value) {
-    return value.trim() === '';
+    return value === '';
 }
 function isValidUrl(url) {
     const urlPattern = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
     return urlPattern.test(url);
 }
-async function shortenUrl() {
-    const url = 'https://cleanuri.com/api/v1/shorten';
-    const payload = {
-        url: 'http://google.com/',
-    };
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(payload),
-        });
-        console.log(response);
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
 function showInvalidMessage(message) {
     linkInput.classList.add('form-invalid');
     invalidMsg.classList.remove('hidden');
     invalidMsg.textContent = message;
-    linkInput.focus();
+    clearInvalidMessage();
 }
 function clearInvalidMessage() {
     timeout = setTimeout(() => {
@@ -57,4 +45,82 @@ function clearInvalidMessage() {
         invalidMsg.classList.add('hidden');
         invalidMsg.textContent = '';
     }, 3000);
+}
+function showLoadingState() {
+    linkInput.setAttribute('disabled', 'true');
+    linkInput.classList.add('disabled');
+    shortenBtn.setAttribute('disabled', 'true');
+    shortenBtn.classList.add('disabled');
+    shortenBtn.textContent = 'Loading...';
+}
+function removeLoadingState() {
+    linkInput.removeAttribute('disabled');
+    linkInput.classList.remove('disabled');
+    shortenBtn.removeAttribute('disabled');
+    shortenBtn.classList.remove('disabled');
+    shortenBtn.textContent = 'Shorten it!';
+}
+async function shortenUrl(longUrl) {
+    const payload = {
+        long_url: longUrl,
+        domain: 'bit.ly',
+    };
+    const options = {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${API_TOKEN}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+    };
+    try {
+        showLoadingState();
+        const response = await fetch(API_URL, options);
+        if (!response.ok) {
+            throw new Error('Something went wrong. Please try again.');
+        }
+        const result = await response.json();
+        const newLinks = {
+            long: result.long_url,
+            short: result.link,
+        };
+        links.push(newLinks);
+        saveShortenUrl();
+        createShortenUrl(newLinks);
+        removeLoadingState();
+    }
+    catch (error) {
+        removeLoadingState();
+        if (error instanceof Error) {
+            showInvalidMessage(error.message);
+        }
+        else {
+            showInvalidMessage('An unknown error occurred.');
+        }
+    }
+}
+function createShortenUrl(link) {
+    const div = document.createElement('div');
+    div.className = 'link-card';
+    div.innerHTML = `
+    <a href="${link.long}" target="_blank" rel="noopener noreferrer" title="${link.long}" class="long-url">
+      ${link.long}
+    </a>
+    <div class="action-container">
+      <a href="${link.short}" target="_blank" rel="noopener noreferrer" title="${link.short}" class="short-url">
+        ${link.short}
+      </a>
+      <button type="button" class="copy-btn">Copy</button>
+    </div>
+      `;
+    linksContainer.classList.remove('hidden');
+    const firstChild = linksContainer.firstChild;
+    linksContainer.insertBefore(div, firstChild);
+}
+function saveShortenUrl() {
+    localStorage.setItem('links', JSON.stringify(links));
+}
+function displayShortenUrl() {
+    const links = localStorage.getItem('links');
+    return links ? JSON.parse(links) : [];
 }
